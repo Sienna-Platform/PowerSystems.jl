e9ce9dbc1a85eef03b28b6f5f2a492454d98b538
Jes Brossman <119698017+jbrossman@users.noreply.github.com>
2026-05-01 10:23:55 -0600

    Delete requested files

--- scripts/format_and_build_docs.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "c:\Users\jstershi\Documents\GitHub\Sienna\PowerSystems.jl"
-julia scripts\format_and_build_docs.jl
-pause
--- scripts/format_and_build_docs.jl
+++
@@ -1,4 +0,0 @@
-include("scripts/formatter/formatter_code.jl")
-using Pkg
-Pkg.activate("docs")
-include("docs/make.jl")
